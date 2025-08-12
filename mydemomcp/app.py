34764830501d1106c98dfa6491d981d39ef0e81e
udara <chamiduudara321@gmail.com>
2025-08-12 14:29:06 +0530

    mcp rag app created with groundx

--- mydemomcp/app.py
+++
@@ -1,5 +0,0 @@
-# the code of the chatbot should be here.
-# there are 3 MCP servers in the .json file.
-# according to the user entering query, the tools are called.
-# all 3 tools related to serching ... ?
-# if a tool needed, go to google and find the json format configuration code.
